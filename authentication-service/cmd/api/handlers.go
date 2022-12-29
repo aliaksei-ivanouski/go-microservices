3d67dbc7195eeeba199afd52037180d584c37ace
Aliaksei Ivanouski <aliakseiivanouski@Aliakseis-MacBook-Pro.local>
2022-12-29 10:08:24 +0400

    add testing

--- authentication-service/cmd/api/handlers.go
+++ authentication-service/cmd/api/handlers.go
@@ -24,13 +24,13 @@ func (app *Config) Authenticate(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	user, err := app.Models.User.GetByEmail(requestPayload.Email)
+	user, err := app.Repo.GetByEmail(requestPayload.Email)
 	if err != nil {
 		tools.ErrorJSON(w, errors.New("Invalid credentials"), http.StatusBadRequest)
 		return
 	}
 
-	valid, err := user.PasswordMatches(requestPayload.Password)
+	valid, err := app.Repo.PasswordMatches(requestPayload.Password, *user)
 	if err != nil || !valid {
 		tools.ErrorJSON(w, errors.New("Invalid credentials"), http.StatusBadRequest)
 		return
@@ -68,8 +68,7 @@ func (app *Config) logRequest(name, data string) error {
 		return err
 	}
 
-	client := &http.Client{}
-	_, err = client.Do(request)
+	_, err = app.Client.Do(request)
 	if err != nil {
 		return err
 	}
